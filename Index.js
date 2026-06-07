8f7c79b90bb2cd07f1f5311e0aa8915c042a3662
tnvi807 <tanvig807@gmail.com>
2026-06-08 02:07:47 +0530

    Initial commit

---
+++ Index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname ,"public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});     
+
