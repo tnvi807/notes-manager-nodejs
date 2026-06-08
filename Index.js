50c1bd1bb51231ede3c1face516bad214c2359c9
tnvi807 <tanvig807@gmail.com>
2026-06-09 04:22:02 +0530

    details is visible

--- Index.js
+++ Index.js
@@ -11,16 +11,22 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     fs.readdir(`./files`,(err,files)=>{
     res.render("index", {files:files});
-    })
-    
+    });   
 });
 
 app.post("/create", (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details , (err) => {
         res.redirect("/");
+    });
+    });
 
+app.get('/file/:filename', (req, res) => {    
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", (err,filedata)=>{
+        res.render("show" ,{filename: req.params.filename , filedata: filedata }); 
     })
-    })
+})
+
+
 
 
 
