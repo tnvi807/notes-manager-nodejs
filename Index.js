b3573627bf978acbdc01f38c8f9d0c28addda63f
tnvi807 <tanvig807@gmail.com>
2026-06-12 23:15:55 +0530

    Trasfer file to mongoDB

--- Index.js
+++ Index.js
@@ -2,48 +2,40 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-const noteModel = require('./models/task')
+const noteModel = require('./models/task');
+const { title } = require('process');
 
 app.use(express.json());
 app.use(express.urlencoded({extends: true}));
 app.use(express.static(path.join(__dirname ,"public")));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    fs.readdir(`./files`,(err,files )=>{
-    res.render("index", {files:files});
+app.get("/", async(req, res) => {
+    let title = await noteModel.find();
+    res.render("index", {title});
     });  
 
-});
 
-app.post("/create", (req, res) => {
-    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details , (err) => {
-        res.redirect("/");
+
+app.post("/create", async (req, res) => {
+    let craetedNote = await noteModel.create({
+      title: req.body.title,
+      details: req.body.details
+    })
+        res.redirect("/")
     });
-    });
 
-app.get('/files/:filename', async (req, res) => {   
-
-    let readUser = await noteModel.find();
+app.get('/read/:title', async (req, res) => {   
+    // find the note by title (assuming title matches filename param)
+    let title = await noteModel.findOne({title: req.params.title});
+    res.render("Show", {title});
         
-        fs.readFile(`./files/${req.params.filename}`, "utf-8" ,(err,filedata)=>{
-        
-            res.render("Show", {
-                    filename:req.params.filename,
-                    filedata:filedata,
-                    readUser
-                   });
-        })
     }); 
 
-app.post("/delete", (req, res) => {
-    fs.unlink(`./files/${req.body.filename}`, (err) => {
-        res.redirect("/");
+app.post("/delete/:id", async (req, res) => {
+    await noteModel.findOneAndDelete({ _id: req.params.id });
+    res.redirect("/");
     });
-    });
-    
-    
-
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- models/task.js
+++ models/task.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 mongoose.connect(`mongodb://127.0.0.1:27017/mongoseenote`)
 
 const noteSchema = mongoose.Schema({
-    task:"string",
-    describtion:"string"
+    title: "string",
+    details: "string"
 })
 
 module.exports = mongoose.model("note", noteSchema);
