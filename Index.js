7afd9201cfe00ee18b143fe595fcf41ca72a9525
tnvi807 <tanvig807@gmail.com>
2026-06-14 01:58:36 +0530

    update added

--- Index.js
+++ Index.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const noteModel = require('./models/task');
-const { title } = require('process');
+
 
 app.use(express.json());
 app.use(express.urlencoded({extends: true}));
 app.use(express.static(path.join(__dirname ,"public")));
 app.set("view engine", "ejs");
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
     let title = await noteModel.find();
     res.render("index", {title});
     });  
@@ -26,16 +26,27 @@ app.post("/create", async (req, res) => {
     });
 
 app.get('/read/:title', async (req, res) => {   
-    // find the note by title (assuming title matches filename param)
-    let title = await noteModel.findOne({title: req.params.title});
-    res.render("Show", {title});
+    let newt = await noteModel.findOne({title: req.params.title});
+    res.render("Show", {title : newt});
         
     }); 
 
-app.post("/delete/:id", async (req, res) => {
-    await noteModel.findOneAndDelete({ _id: req.params.id });
+app.get("/delete/:id", async (req, res) => {
+   let title = await noteModel.findOneAndDelete({ _id: req.params.id });
     res.redirect("/");
     });
+
+app.get("/edit/:id", async (req,res) =>{
+    let title = await noteModel.findOne({_id: req.params.id })
+    res.render("edit", {title})
+});
+
+app.post("/update/:id" , async (req,res) =>{
+    console.log("UPDATE ROUTE HIT");
+    let {title , details} = req.body;
+    let titles = await noteModel.findOneAndUpdate({_id: req.params.id }, {title , details},{new:true})
+    res.redirect(`/read/${titles.title}`);
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
